9ab2e1726e1618a97fbe39e392579aa148f5c8af
Ibnballo1 <belloabdullateef035@gmail.com>
2022-10-05 23:10:36 +0100

    Add homepage

--- src/components/Continent.js
+++ src/components/Continent.js
@@ -1,29 +1,50 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import { useSelector } from 'react-redux';
+// import Countries from './Countries';
 
 const Continent = () => {
   const stateData = useSelector((state) => state.corona);
-  const newStateData = stateData.slice(0, 10);
+  const newStateData = stateData.slice(0, 50);
   const mostConfirmed = Math.max(...newStateData.map((confirmedCases) => confirmedCases.confirmed));
-  const mostAffectedContinent = newStateData.map((confirmedCases, index) => {
-    const { continent, confirmed } = confirmedCases;
-    return (
-      <h2 key={index}>
-        {(mostConfirmed === confirmed) && continent}
-      </h2>
-    );
-  });
-  // console.log('Most confirmed cases ', mostConfirmed);
+
+  let mostAffectedContinent = '';
+  for (let i = 0; i < newStateData.length; i += 1) {
+    if (mostConfirmed === newStateData[i].confirmed) {
+      mostAffectedContinent = newStateData[i].continent;
+    }
+  }
+
+  const countries = [];
+  const affected = [];
+  // Getting the countries of the most affected
+  for (let i = 0; i < newStateData.length; i += 1) {
+    if (newStateData[i].continent === mostAffectedContinent) {
+      countries.push(newStateData[i].country);
+      affected.push(newStateData[i].confirmed);
+    }
+  }
 
   return (
     <div>
       <div>
-        {mostAffectedContinent}
-        <p>
-          {mostConfirmed}
-        </p>
+        <h2>{mostAffectedContinent}</h2>
+        <p>{mostConfirmed}</p>
       </div>
+      <section>
+        <h5>STATS BY COUNTRY</h5>
+        <div>
+          {
+            countries.map((country, index) => (
+              <div key={index}>
+                {country}
+                <p>{affected[index]}</p>
+              </div>
+            ))
+          }
+        </div>
+      </section>
     </div>
   );
 };
---
+++ src/components/Countries.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+
+// const Countries = () => {
+//   const stateData = useSelector((state) => state.corona);
+//   const newStateData = stateData.slice(0, 10);
+//   console.log(newStateData);
+//   return (
+//     <div>
+//       <h5>STATS BY COUNTRY</h5>
+//     </div>
+//   );
+// };
+
+// export default Countries;
